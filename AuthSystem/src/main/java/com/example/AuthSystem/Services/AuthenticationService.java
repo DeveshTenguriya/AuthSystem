70b3d6790e4cbd25c9dc8ee085e5ba1f32fa8f67
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-02 13:29:34 +0530

    complete the AuthenticationService now ready to check the api request

--- AuthSystem/src/main/java/com/example/AuthSystem/Services/AuthenticationService.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/Services/AuthenticationService.java
@@ -1,4 +1,75 @@
 package com.example.AuthSystem.Services;
 
+import com.example.AuthSystem.Config.JwtServices;
+import com.example.AuthSystem.DTO.AuthResponse;
+import com.example.AuthSystem.DTO.LoginRequest;
+import com.example.AuthSystem.Entity.RefreshToken;
+import com.example.AuthSystem.Entity.User;
+import com.example.AuthSystem.Repository.RefreshTokenRepository;
+import com.example.AuthSystem.Repository.UserRepository;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtServices jwtServices;
+    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationService(AuthenticationManager authenticationManager, JwtServices jwtServices, UserRepository userRepository, RefreshTokenRepository refreshTokenRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtServices = jwtServices;
+        this.userRepository = userRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    public AuthResponse login(LoginRequest request){
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getPassword(),
+                        request.getEmail())
+        );
+
+        User user=userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow();
+
+        String accessToken=
+                jwtServices.generateToken(
+                        new org.springframework.security.core.userdetails.User(
+                                user.getEmail(),
+                                user.getPassword(),
+                                List.of()
+                        )
+                );
+
+        String refreshToken = UUID.randomUUID().toString();
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .token(refreshToken)
+                        .user(user)
+                        .expiryDate(LocalDateTime.now().plusDays(7))
+                        .revoked(false)
+                        .build()
+        );
+
+        return new AuthResponse(accessToken, refreshToken);
+    }
 }
