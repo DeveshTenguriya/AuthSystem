cc42ea94451046a3c6c231aa63caae85a9a5e596
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-02 12:03:19 +0530

    validate the token and check the security with auth and continue the filter chain with its request and response api request

--- AuthSystem/src/main/java/com/example/AuthSystem/Config/JwtAuthenticationFilter.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/Config/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -44,8 +45,22 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             UserDetails userDetails=
                     userDetailsService
                     .loadUserByUsername(username);
+
+
+          if (jwtServices.validateToken(token,userDetails)){
+              UsernamePasswordAuthenticationToken authToken=
+                      new UsernamePasswordAuthenticationToken(
+                              userDetails,
+                              null,
+                              userDetails.getAuthorities());
+
+              SecurityContextHolder.getContext()
+                      .setAuthentication(authToken);
+          }
         }
+
+        filterChain.doFilter(request,response);
     }
 
-    
+
 }
