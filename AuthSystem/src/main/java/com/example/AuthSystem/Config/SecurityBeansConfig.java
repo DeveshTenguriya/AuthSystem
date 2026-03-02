0aef18ac86e03a322061f8b0fc44b3cc605c10dc
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-02 12:25:01 +0530

    update the SecurityBeansConfig for proper authentication method and set session and filter before auth tpo check validity of the user

--- AuthSystem/src/main/java/com/example/AuthSystem/Config/SecurityBeansConfig.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/Config/SecurityBeansConfig.java
@@ -1,19 +1,33 @@
 package com.example.AuthSystem.Config;
 
+import org.springframework.boot.web.server.servlet.Session;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 public class SecurityBeansConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityBeansConfig(JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable()).sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpSecurityHttpBasicConfigurer -> {});
+                .addFilterBefore(jwtFilter ,
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
