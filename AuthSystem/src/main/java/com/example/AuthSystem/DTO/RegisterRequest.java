0723f0fcafb07f012964ac7481255bae3cb4159d
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-04 01:49:30 +0530

    created the dto RegisterRequest for register the user to the database

---
+++ AuthSystem/src/main/java/com/example/AuthSystem/DTO/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.example.AuthSystem.DTO;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "username is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
+    private String email;
+
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
