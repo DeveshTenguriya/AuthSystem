09059071578a91d898d4c51b572d5f51d4e2ba90
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-03 22:40:02 +0530

    added the authentication manager as it not in the inbuilt spring any more and declared the filterchain as bean so spring can read it properly

--- AuthSystem/src/main/java/com/example/AuthSystem/Config/SecurityBeansConfig.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/Config/SecurityBeansConfig.java
@@ -1,10 +1,15 @@
 package com.example.AuthSystem.Config;
 
 import org.springframework.boot.web.server.servlet.Session;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,6 +22,7 @@ public class SecurityBeansConfig {
         this.jwtFilter = jwtFilter;
     }
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
@@ -24,6 +30,8 @@ public class SecurityBeansConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/users/**").hasAnyRole("USER","ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter ,
@@ -31,4 +39,12 @@ public class SecurityBeansConfig {
 
         return http.build();
     }
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+
 }
