657756758f5e6688c3c4a54b821144e718a9126d
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-06 23:14:02 +0530

    remove the unwanted dependency

--- AuthSystem/src/main/java/com/example/AuthSystem/DTO/RegisterRequest.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/DTO/RegisterRequest.java
@@ -1,6 +1,6 @@
 package com.example.AuthSystem.DTO;
 
-import jakarta.persistence.Column;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
