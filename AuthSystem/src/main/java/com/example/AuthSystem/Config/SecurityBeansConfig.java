44a17a6643f2a83f086d46d1957584e7a782f027
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-06 23:17:20 +0530

    remove the unwanted dependency

--- AuthSystem/src/main/java/com/example/AuthSystem/Config/SecurityBeansConfig.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/Config/SecurityBeansConfig.java
@@ -1,6 +1,6 @@
 package com.example.AuthSystem.Config;
 
-import org.springframework.boot.web.server.servlet.Session;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
