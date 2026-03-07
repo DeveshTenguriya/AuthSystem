1bc67daecb5231b60bb174fd7b3d2c2a9662afc6
Devesh Tenguriya <deveshtenguriya8220@gmail.com>
2026-03-07 23:15:35 +0530

    delete the unwanted dependency from the authticationservices

--- AuthSystem/src/main/java/com/example/AuthSystem/Services/AuthenticationService.java
+++ AuthSystem/src/main/java/com/example/AuthSystem/Services/AuthenticationService.java
@@ -11,7 +11,6 @@ import com.example.AuthSystem.Repository.RefreshTokenRepository;
 import com.example.AuthSystem.Repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
